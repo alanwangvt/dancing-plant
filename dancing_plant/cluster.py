287a8d109eaace83fa7385b71cac23e7d2fb53b8
Lowell Weissman <lowell6@vt.edu>
2021-05-22 13:06:48 -0400

    made trace clustering user-friendly via cluster_trace.py

--- dancing_plant/cluster.py
+++ dancing_plant/cluster.py
@@ -8,26 +8,27 @@ import numpy as np
 import cv2
 from tslearn.clustering import TimeSeriesKMeans
 
-from track import draw_trace
+import dancing_plant
+from dancing_plant.track import draw_trace
+from dancing_plant.track import out_pfx as track_out_pfx
 
-trace_path = "/home/lowell/dancing-plant/RAFT"  # assumes traces in format X_trace{i}.csv and Y_trace{i}.csv where 'i' is partition
-out_path = "/home/lowell/dancing-plant/RAFT/dtw_corr_traces"
-image_path = "/home/lowell/dancing-plant/DPI/05-04-2021/indexed/000.jpg"  # for extraction display
+TRACE_PATH = "/home/lowell/dancing-plant/RAFT"  # assumes traces in format X_trace{i}.csv and Y_trace{i}.csv where 'i' is partition
+OUT_PATH = "/home/lowell/dancing-plant/RAFT/dtw_corr_traces"
+IMAGE_PATH = "/home/lowell/dancing-plant/DPI/05-04-2021/indexed/000.jpg"  # for extraction display
 
 NUM_PART = 2
 NUM_CLUSTER = (4, 4)  # num clusters for each k-means run (one for each partition)
 NUM_TRACE = 3  # Top NUM_TRACE most mobile corner traces will be kept for EACH cluster
 
 
-if __name__ == "__main__":
-    
+def cluster(trace_path, out_path, image_path, num_part, num_cluster, num_trace):
     # load trace
-    if NUM_PART is None or NUM_PART <= 1:
+    if num_part is None or num_part <= 1:
         x_split_paths = [osp.join(trace_path, "X_trace.csv")]
         y_split_paths = [osp.join(trace_path, "Y_trace.csv")]
     else:
-        x_split_paths = [osp.join(trace_path, f"X_trace{idx}.csv") for idx in range(NUM_PART)]
-        y_split_paths = [osp.join(trace_path, f"Y_trace{idx}.csv") for idx in range(NUM_PART)]
+        x_split_paths = [osp.join(trace_path, f"X_trace{idx}.csv") for idx in range(num_part)]
+        y_split_paths = [osp.join(trace_path, f"Y_trace{idx}.csv") for idx in range(num_part)]
 
     x_split_traces = [np.loadtxt(fname, dtype=np.uint32, delimiter=",") for fname in x_split_paths]
     y_split_traces = [np.loadtxt(fname, dtype=np.uint32, delimiter=",") for fname in y_split_paths]
@@ -41,7 +42,7 @@ if __name__ == "__main__":
 
     # time-series cluster each x split with dynamic time warping
     # this scales horribly with both number of time series and time series length, so shouldn't use more than maybe 25 traces per split
-    for idx, pack in enumerate(zip(NUM_CLUSTER, x_split_traces, y_split_traces)):
+    for idx, pack in enumerate(zip(num_cluster, x_split_traces, y_split_traces)):
         K, xt, yt = pack
 
         # create xy-coordinate stack
@@ -59,18 +60,21 @@ if __name__ == "__main__":
         model.fit(xyt)
 
         # extract fastest moving trace for each cluster
-        extractions = np.empty_like(xyt, shape=(K, NUM_TRACE, sz, d))
+        extract_list = []
         cluster_labels = model.predict(xyt)
         xyt += offset  # fix offset
         for k in range(K):
             cluster_traces = xyt[cluster_labels == k]
-            extractions[k, :, :, :] = cluster_traces[:NUM_TRACE]  # take top NUM_TRACE fastest moving traces
+            num_extract = min(num_trace, len(cluster_traces))
+
+            extractions = cluster_traces[:num_extract]  # take top fastest moving traces
+            extract_list.append(extractions)
 
             # save extracted traces
-            ex_xt = extractions[k, :, :, 1]
-            ex_yt = extractions[k, :, :, 0]
-            np.savetxt(osp.join(out_path, f"Y_trace{idx}-K{k}-top{NUM_TRACE}.csv"), ex_yt, fmt="%d", delimiter=",")
-            np.savetxt(osp.join(out_path, f"X_trace{idx}-K{k}-top{NUM_TRACE}.csv"), ex_xt, fmt="%d", delimiter=",")
+            ex_xt = extractions[:, :, 1]
+            ex_yt = extractions[:, :, 0]
+            np.savetxt(osp.join(out_path, f"Y_trace{idx}-K{k}-top{num_extract}.csv"), ex_yt, fmt="%d", delimiter=",")
+            np.savetxt(osp.join(out_path, f"X_trace{idx}-K{k}-top{num_extract}.csv"), ex_xt, fmt="%d", delimiter=",")
 
             # display full cluster
             img_k = img.copy()
@@ -79,6 +83,62 @@ if __name__ == "__main__":
 
         # display all extracted traces together
         img_e = img.copy()
-        extractions = extractions.reshape(-1, sz, d)  # flatten cluster and trace idx dims
-        draw_trace(img_e, extractions)
-        cv2.imwrite(osp.join(out_path, f"track{idx}-all-top{NUM_TRACE}-each.jpg"), img_e)
+        all_extractions = np.concatenate(extract_list, axis=0)
+        draw_trace(img_e, all_extractions)
+        cv2.imwrite(osp.join(out_path, f"track{idx}-all-tops.jpg"), img_e)
+
+
+def out_pfx(file_name):
+    """Prepend file name with fixed output prefix."""
+    pfx = osp.join(osp.dirname(osp.dirname(dancing_plant.__file__)), "clustered")
+    if not osp.exists(pfx):
+        os.makedirs(pfx)
+    return osp.join(pfx, file_name)
+
+
+def get_num_part(trace_path):
+    file_names = os.listdir(trace_path)
+    trace_key = "_trace"
+    trace_names = filter(lambda s: s[1:].startswith(trace_key), file_names)
+    part_idxs = [int(x.split(trace_key)[-1][0]) for x in trace_names]
+    return max(part_idxs) + 1
+
+
+def run_cluster_with_defaults(image_path, num_cluster, num_trace):
+    trace_path = track_out_pfx("")
+
+    if not os.listdir(trace_path):
+        raise EnvironmentError("No trace CSVs found at expected trace path: "
+            f"{trace_path}\nRun gen_flow.py and gen_trace.py first.")
+
+    num_part = get_num_part(trace_path)
+
+    if type(num_cluster) is int:
+        num_cluster = [num_cluster] * num_part
+
+    if num_part != len(num_cluster):
+        raise ValueError(f"Found {num_part} partitions, but only "
+            f"specified {len(num_cluster)} cluster quantities. Need "
+            f"one cluster quantity per partition.")
+
+    cluster(
+        trace_path, 
+        out_pfx(""), 
+        image_path, 
+        num_part, 
+        num_cluster, 
+        num_trace
+    )
+
+
+
+if __name__ == "__main__":
+    cluster(
+        TRACE_PATH, 
+        OUT_PATH, 
+        IMAGE_PATH, 
+        NUM_PART, 
+        NUM_CLUSTER, 
+        NUM_TRACE
+    )
+    
---
+++ launch/cluster_trace.py
@@ -0,0 +1,61 @@
+"""
+Use time-series clustering to group traces based
+on their movement patterns.
+
+This assumes trace CSVs have already been generated
+using gen_trace.py (which itself requires running
+gen_flow.py). These CSVs will be located at
+<root>/tracks.
+
+Output CSVs and JPEGs are dumped at <root>/clustered.
+For each partition index <idx>, we generate a image
+of each cluster 'track<idx>-K<cluster_idx>.jpg,
+and one image of all the extractions, where the top
+'num_trace' fastest are taken from each cluster.
+If a cluster has less than 'num_trace' traces, all
+traces are extracted. 
+
+CSVs follow a similar naming scheme but are labelled
+with the number of top traces successfully extracted.
+
+Parameters:
+    disp_img_path - image to display generated traces on for visualization purposes (e.g. first image of experiment)
+    num_cluster   - number of clusters for each partition; if an integer this value is used for all partitions,
+                    otherwise use a tuple with length equal to number of partitions in <root>/tracks
+    num_trace     - the number of top traces to keep in each CLUSTER of each partition, sorted by cumulative absolute movement over time
+
+Example:
+    Say we generated traces using gen_trace.py for an experiment with two plant
+    partitions, and want their traces to be grouped into three and five clusters,
+    respectively, after quick visual analysis of the traces. Further, we want
+    to keep at most four traces per cluster per partition. And our original
+    frames are kept at '/a/b/c/experiment'. We would use...
+
+    disp_img_path = "/a/b/c/experiment/000.jpg"
+
+    num_cluster = (3, 5)
+
+    num_trace = 4
+
+"""
+
+###############################
+### MODIFY PARAMETERS BELOW ###
+
+disp_img_path = "/home/lowell/dancing-plant/DPI/05-18-2021a/indexed/00.jpg"
+
+num_cluster = 3
+
+num_trace = 5
+
+### SHOULD NOT NEED TO MODIFY BELOW ###
+#######################################
+
+
+if __name__ == "__main__":
+    from dancing_plant.cluster import run_cluster_with_defaults
+    run_cluster_with_defaults(
+        disp_img_path,
+        num_cluster,
+        num_trace
+    )
