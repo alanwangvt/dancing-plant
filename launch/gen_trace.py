d4c34ba749da07d469f1d8be12fca6a197850227
Alan Wang <alanwang@vt.edu>
2022-01-25 15:15:33 -0500

    Experiment: dataset01/20210623AT/4

--- launch/gen_trace.py
+++ launch/gen_trace.py
@@ -98,9 +98,9 @@ Example:
 just_anchors = False
 show_partition = True
 
-flow_dir = "/work/alanwang/dataset01/20210623AT/3/raft-flow-raw-1"
+flow_dir = "/work/alanwang/dataset01/20210623AT/4/raft-flow-raw-1"
 
-disp_img_path = "/work/alanwang/dataset01/20210623AT/3/3-001.jpg"
+disp_img_path = "/work/alanwang/dataset01/20210623AT/4/4-001.jpg"
 
 x_splits = ()
 y_splits = ()
