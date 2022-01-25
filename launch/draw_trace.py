86a8cc2a951af653dd987e8b1bb099dcd77419c3
Alan Wang <alanwang@vt.edu>
2022-01-25 09:09:02 -0500

    testing

--- launch/draw_trace.py
+++ launch/draw_trace.py
@@ -43,7 +43,7 @@ Example:
 ###############################
 ### MODIFY PARAMETERS BELOW ###
 
-experiment_dir = "/work/alanwang/dataset01/20210621BT/0/"
+experiment_dir = "/work/alanwang/dataset01/20210623AT/0/"
 
 trace_path = "/home/alanwang/dancing-plant/tracks/"
 
