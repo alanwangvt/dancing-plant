02faf99508f483e4e61f4ce71c36c0362466ae02
Alan Wang <alanwang@vt.edu>
2022-02-03 09:24:26 -0500

    Experiment: gen_flow dataset04 sample_freq=4

--- clean_up.sh
+++ clean_up.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
-mv trace_cache $1
-mv tracks $1
-mv clustered $1
-mv annotated $1
+# mv trace_cache $1
+# mv tracks $1
+# mv clustered $1
+# mv annotated $1
 
 # rm trace_cache/* tracks/* clustered/* annotated/*
+mv 
